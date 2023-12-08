9a00bb047bbd20f6f2d73e183e18c50c5d25ec04
KeyronLinarez <keyronlinarez@gmail.com>
2023-12-08 08:13:08 -0800

    Shooting bug fix, fixed score board

--- examples/simple-game-2/src/main.rs
+++ examples/simple-game-2/src/main.rs
@@ -7,9 +7,12 @@ use rand::Rng;
 const W: f32 = 320.0;
 const H: f32 = 640.0;
 const GUY_SPEED: f32 = 4.0;
+const BULLET_SPEED: f32 = 48.0;
 const SPRITE_MAX: usize = 32;
 const CATCH_DISTANCE: f32 = 16.0;
 const COLLISION_STEPS: usize = 3;
+const SCROLL_SPEED: f32 = 1.0;
+const accumulator: f32 = H + 8.0;
 struct Guy {
     pos: Vec2,
 }
@@ -28,9 +31,11 @@ struct Game {
     screens_loaded: f32,
     guy: Guy,
     asteroids: Vec<Asteroid>,
+    bullets: Vec<Asteroid>,
     asteroid_timer: u32,
     lives: u32,
     font: engine_simple::BitFont,
+    accumulator: f32,
 }
 
 impl engine::Game for Game {
@@ -116,12 +121,16 @@ impl engine::Game for Game {
             screens_loaded: 1.0,
             walls: vec![left_wall, right_wall, floor],
             asteroids: Vec::with_capacity(25),
+            bullets: Vec::with_capacity(25),
             asteroid_timer: 0,
             lives: 30,
             font,
+            accumulator: 0.0,
         }
     }
     fn update(&mut self, engine: &mut Engine) {
+        // check if shoot button pressed
+        let shooting = engine.input.is_key_pressed(engine::Key::Space);
         let dir = engine.input.key_axis(engine::Key::Left, engine::Key::Right);
         self.guy.pos.x += dir * GUY_SPEED;
         let mut contacts = Vec::with_capacity(self.walls.len());
@@ -176,7 +185,32 @@ impl engine::Game for Game {
                     // so far it seems resolved; for multiple guys this should probably set a flag on the guy
                 }
             }
+        }  
+        //
+        // 
+        //
+        // timer logic for shooting
+        if shooting {
+            self.bullets.push(Asteroid {
+                pos: Vec2 {
+                    x: self.guy.pos.x,
+                    y: self.guy.pos.y,
+                },
+                vel: Vec2 {
+                    x: 0.0,
+                    y: BULLET_SPEED,
+                },
+            });
         }
+        for bullets in self.bullets.iter_mut() {
+            bullets.pos += bullets.vel;
+        }
+        //
+        //
+        //
+        //
+
+        // timer logic for asteroids
         let mut rng = rand::thread_rng();
         if self.asteroid_timer > 0 {
             self.asteroid_timer -= 1;
@@ -197,6 +231,7 @@ impl engine::Game for Game {
         for asteroid in self.asteroids.iter_mut() {
             asteroid.pos += asteroid.vel;
         }
+        // COLLISION FOR ASTEROIDS
         if let Some(idx) = self
             .asteroids
             .iter()
@@ -205,30 +240,34 @@ impl engine::Game for Game {
             self.asteroids.swap_remove(idx);
             self.lives -= 1;
         }
-        self.asteroids.retain(|asteroid| asteroid.pos.y > -8.0)
+        self.asteroids.retain(|asteroid| asteroid.pos.y > -8.0);
+
+        // collisions for bullets
+
     }
     fn render(&mut self, engine: &mut Engine) {
 
         // scrolling camera code
-        self.camera.screen_pos[1] += 1.0;
-        self.guy.pos.y += 1.0;
+        // self.camera.screen_pos[1] += 1.0;
+        self.camera.screen_pos[1] += SCROLL_SPEED;
+        self.guy.pos.y += SCROLL_SPEED;
 
-        // camera following ship
+        // camera following ship slowly
         // in 400 x 600 resolution, this V slows horizontal scrolling -->                         offset so you start in center
         self.camera.screen_pos[0] = (self.guy.pos.x / 2.0) - (self.camera.screen_size[0] / 2.0) + 80.0;
 
         // infinite background code
-        println!("{}", self.camera.screen_pos[1]);
-        println!("WERID MATH = {}", ((320.0 * self.screens_loaded) as usize));
+        //println!("{}", self.camera.screen_pos[1]);
+        //println!("WERID MATH = {}", ((320.0 * self.screens_loaded) as usize));
         // once you hit the thresehold approaching end of first sreen
         if (self.screen_buffer && (self.camera.screen_pos[1] + 320.0) as usize % ((320.0 * self.screens_loaded) as usize) == 0){
-            println!("YOU JUST HIT THE SCROLLING THRESHOLD");
+            //println!("YOU JUST HIT THE SCROLLING THRESHOLD");
             self.vertical_screen += 4.0;
             self.screens_loaded += 1.0;
             self.screen_buffer = false;
         } else if (self.camera.screen_pos[1] as usize % ((640.0 * self.screens_loaded) as usize) == 0){
             self.scrolling_screen += 4.0;
-            println!("222222222222222222222222D");
+            //println!("222222222222222222222222D");
             // was one when almost worked
             self.screens_loaded += 1.0;
             self.screen_buffer = true;
@@ -329,20 +368,42 @@ impl engine::Game for Game {
         .into();
         // TODO animation frame
         uvs[guy_idx] = SheetRegion::new(0, 2480, 0, 8, 207, 206);
-        // set asteroids
-        let asteroid_start = guy_idx + 1;
-        for (asteroid, (trf, uv)) in self.asteroids.iter().zip(
-            trfs[asteroid_start..]
+
+        // set bullets
+        let bullet_start = guy_idx + 1;
+        for (bullets, (trf, uv)) in self.bullets.iter().zip(
+            trfs[bullet_start..]
                 .iter_mut()
-                .zip(uvs[asteroid_start..].iter_mut()),
+                .zip(uvs[bullet_start..].iter_mut()),
         ) {
             *trf = AABB {
-                center: asteroid.pos,
+                center: Vec2 { x: bullets.pos.x, y: bullets.pos.y},
                 size: Vec2 { x: 16.0, y: 16.0 },
             }
             .into();
             *uv = SheetRegion::new(0, 0, 1622, 4, 16, 15);
         }
+
+        let bullet_end = bullet_start + self.bullets.len();
+
+        // add this to the ateroids pos val
+        self.accumulator += SCROLL_SPEED;
+        // set asteroids
+        let asteroid_start = bullet_end;
+        for (asteroid, (trf, uv)) in self.asteroids.iter().zip(
+            trfs[asteroid_start..]
+                .iter_mut()
+                .zip(uvs[asteroid_start..].iter_mut()),
+        ) {
+            println!("ASTEROID acumulator: {}", self.accumulator);
+            *trf = AABB {
+                center: Vec2 { x: asteroid.pos.x, y: asteroid.pos.y + self.accumulator},
+                size: Vec2 { x: 16.0, y: 16.0 },
+            }
+            .into();
+            *uv = SheetRegion::new(0, 0, 1622, 4, 16, 15);
+        }
+
         let sprite_count = asteroid_start + self.asteroids.len();
         let lives_str = self.lives.to_string();
         let text_len = lives_str.len();
@@ -357,8 +418,8 @@ impl engine::Game for Game {
             sprite_count,
             &lives_str,
             Vec2 {
-                x: 16.0,
-                y: H - 16.0,
+                x: 16.0 + self.camera.screen_pos[0],
+                y: H - 16.0 + self.accumulator,
             }
             .into(),
             16.0,
